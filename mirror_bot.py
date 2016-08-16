61b92d1724865b01a687c1bcecfec83c50a5cc1d
Bhavish Yalamanchi <bhavishy6@gmail.com>
2016-08-16 11:20:23 -0400

    fixed praw installation, testing

--- mirror_bot.py
+++ mirror_bot.py
@@ -1,8 +1,9 @@
+import praw
 import re
 
 def mirrors_requested(c):
     text = c.body()
-    if text.find("find mirrors") or text.find("all mirros"):
+    if text.find("find mirrors") or text.find("all mirrors"):
         return True
 
 
@@ -36,12 +37,15 @@ def check_comment_for_mirrors(text):
     return links
 
 
-if __name__ is '__main__':
-    import praw
-    r = praw.Reddit(user_agent);
-    user_agent = "Mirror Finder 1.0 by u/bhavishy6"
+if __name__ == '__main__':
+    user_agent = ("Mirror Finder 1.0 by u/bhavishy6")
+    r = praw.Reddit(user_agent=user_agent,client_id='Mpv45Va9NRiFJQ',
+                        client_secret='tbk0w4c0chto1YVHYW7Tch0EmaM',
+                        redirect_url='http://127.0.0.1:65010/authorize_callback')
 
     r.login()
-    for c in praw.helpers.comment_stream(r, 'hiphopheads'):
-        if mirrors_requested(c):
-            bot_action(c)
+    while True:
+        for c in praw.helpers.comment_stream(r, 'hiphopheads'):
+            if check_comment_for_mirrors(c.body):
+                print "Mirror Found..."
+                bot_action(c)
